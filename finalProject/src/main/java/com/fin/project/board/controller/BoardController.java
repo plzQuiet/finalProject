738a0ac85a08650fcc18609e203d84ea61296705
jiny <rudwls2938@naver.com>
2024-09-06 17:54:59 +0900

    유경진 -board 백앤드 폴더 생성

--- finalProject/src/main/java/com/fin/project/board/controller/BoardController.java
+++ finalProject/src/main/java/com/fin/project/board/controller/BoardController.java
@@ -1,9 +1,19 @@
 package com.fin.project.board.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.fin.project.board.model.service.BoardService;
+
+@SessionAttributes("loginMember")
+@RequestMapping("/board")
 @Controller
 public class BoardController {
+	
+	@Autowired
+	private BoardService service;
 
 
 }
--- finalProject/src/main/java/com/fin/project/board/model/dto/Board.java
+++ finalProject/src/main/java/com/fin/project/board/model/dto/Board.java
@@ -1,5 +1,37 @@
 package com.fin.project.board.model.dto;
 
-public class Board {
+import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Board {
+	
+	private int boardNo;
+	private String boardTitle;
+	private String boardContent;
+	private String boardCreateDate;
+	private String boardUpdateDate;
+	private int readCount;
+	private String boardDeleteFlag;
+	private String boardScreatFlag;
+	
+	private int commentCount; 
+
+	// 회원 
+	private int memberNo;
+	private String memberName;
+
+	// == boardCode
+	private int categoryCode;
+	
+	// 이미지
+	private List<BoardImage> imageList;
+	
+	// 댓글
+	private List<Comment> commentList;
 }
