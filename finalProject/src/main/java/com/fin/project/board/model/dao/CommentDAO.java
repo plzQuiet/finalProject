8fa4ec53b296e4ba014b4f29673d8cc0e29b3e96
jiny <rudwls2938@naver.com>
2024-09-07 15:46:35 +0900

    유경진 - dao 수정 및 commentDAO 파일 생성

--- finalProject/src/main/java/com/fin/project/board/model/dao/CommentDAO.java
+++ finalProject/src/main/java/com/fin/project/board/model/dao/CommentDAO.java
@@ -5,10 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class BoardDAO {
+public class CommentDAO {
 	
 	@Autowired
 	private SqlSessionTemplate sqlSession;
-
 
 }
--- finalProject/src/main/java/com/fin/project/board/model/dto/BoardImage.java
+++ finalProject/src/main/java/com/fin/project/board/model/dto/BoardImage.java
@@ -1,5 +1,13 @@
 package com.fin.project.board.model.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class BoardImage {
+
 
 }
