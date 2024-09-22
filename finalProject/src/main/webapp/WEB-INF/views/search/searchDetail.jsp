a4ffc75b7203e4ab4abab024d743442cf3ec0669
재원 <jw140@naver.com>
2024-09-22 18:45:55 +0900

    이재원 - 상세 검색 기능 추가

--- finalProject/src/main/webapp/WEB-INF/views/search/searchDetail.jsp
+++ finalProject/src/main/webapp/WEB-INF/views/search/searchDetail.jsp
@@ -10,86 +10,81 @@
 <link rel="stylesheet" href="/resources/css/main-style.css">
 </head>
 <body>
-	
-		<jsp:include page="/WEB-INF/views/common/header.jsp"></jsp:include>
-		<section class="main">
-			<jsp:include page="/WEB-INF/views/search/sideMenu.jsp"></jsp:include>
+	<jsp:include page="/WEB-INF/views/common/header.jsp"></jsp:include>
+	<section class="main">
+		<jsp:include page="/WEB-INF/views/search/sideMenu.jsp"></jsp:include>
 
-			<section class="main-content">
-				<div class="main-title">상세 검색</div>
-				<section class="body-content">
-					<div class="searchFormDetail">
-						<div class="detailItem">
-							<div class="details">검색 항목</div>
-							<div class="detailsYear">발행 연도</div>
-							<div class="detailsAlign">정렬 조건</div>
-							<div class="detailsCount">출력 건수</div>
+		<section class="main-content">
+			<div class="main-title">상세 검색</div>
+			<section class="body-content">
+				<div class="searchFormDetail">
+					<div class="detailItem">
+						<div class="details">검색 항목</div>
+						<div class="detailsYear">발행 연도</div>
+						<div class="detailsCount">출력 건수</div>
+						<div class="detailsAlign"></div>
 
-						</div>
-						<div class="detailInput">
+					</div>
+					<div class="detailInput">
 
-							<div class="searchDetail">
-								<div class="searchTitle">
-									<div class="bookTitle">서명</div>
-									<div class="input">
-										<input type="text" id="inputDetail" placeholder="책 제목을 입력해주세요."></input>
-									</div>
+						<div class="searchDetail">
+							<div class="searchTitle">
+								<div class="bookTitle">서명</div>
+								<div class="input">
+									<input type="text" class="inputDetail" id="inputDetail" placeholder="책 제목을 입력해주세요."></input>
 								</div>
+							</div>
 
-								<div class="searchAuthor">
-									<div class="author">저자</div>
-									<div class="input">
-										<input type="text" id="inputDetail" placeholder="저자 이름을 입력해주세요."></input>
-									</div>
+							<div class="searchAuthor">
+								<div class="author">저자</div>
+								<div class="input">
+									<input type="text" class="inputDetail" id="inputDetail" placeholder="저자 이름을 입력해주세요."></input>
 								</div>
+							</div>
 
-								<div class="searchPublisher">
-									<div class="publisher">출판사</div>
-									<div class="input">
-										<input type="text" id="inputDetail" placeholder="출판사명을 입력해주세요."></input>
-									</div>
+							<div class="searchPublisher">
+								<div class="publisher">출판사</div>
+								<div class="input">
+									<input type="text" class="inputDetail" id="inputDetail" placeholder="출판사명을 입력해주세요."></input>
 								</div>
+							</div>
 
-								<div class="searchYear">
-									<div class="inputYear">
-										<input type="text" id="inputYear"></input> 
-										<span>부터 </span>
-										<input type="text" id="inputYear"></input>
-									</div>
+							<div class="searchYear">
+								<div class="inputYear">
+									<input type="text" class="inputDetail" id="inputYear"></input> 
+									<span>부터 </span>
+									<input type="text" class="inputDetail" id="inputYear"></input>
 								</div>
+							</div>
 
-								<div class="searchAlign">
-									<select id="searchAlign" name="searchAlign" title="정렬조건">
-										<option value="accuracy">정확도순</option>
-										<option value="recent">최근순</option>
-										<option value="none">선택 안함</option>
-									</select>
-								</div>
-
-								<div class="searchPrint">
-									<select id="searchPrint" name="searchPrint" title="출력건수">
-										<option value="10">10건</option>
-										<option value="20">20건</option>
-										<option value="30">30건</option>
-									</select>
-								</div>
+							<div class="searchPrint">
+								<select id="searchPrint" name="searchPrint" title="출력건수">
+									<option value="10">10건</option>
+									<option value="20">20건</option>
+									<option value="30">30건</option>
+								</select>
 							</div>
 						</div>
 					</div>
-					<div class="btn-area">
-						<div class="index-btn">
-							<button id="index-btn">검색</button>
-						</div>
-
-						<div class="refresh-btn">
-							<button id="refresh-btn">초기화</button>
-						</div>
+				</div>
+				<div class="btn-area">
+					<div class="index-btn">
+						<button id="index-btn">검색</button>
 					</div>
-				</section>
-			</section>
 
+					<div class="refresh-btn">
+						<button id="refresh-btn">초기화</button>
+					</div>
+				</div>
+				<div id="queryResult"></div>
+			</section>
 		</section>
+	</section>
+	<jsp:include page="/WEB-INF/views/search/searchModal.jsp"></jsp:include>
+    <script>
+        const loginMemberNo = "${loginMember.memberNo}";
+    </script>
+    <script src="/resources/js/search/search.js"></script>
 	<jsp:include page="/WEB-INF/views/common/footer.jsp" />
-	
 </body>
 </html>
--- finalProject/src/main/webapp/resources/js/search/search.js
+++ finalProject/src/main/webapp/resources/js/search/search.js
@@ -1,6 +1,8 @@
 const query = document.getElementById("query");
 const queryResult = document.getElementById("queryResult");
 const searchBtn = document.getElementById("searchBtn");
+const searchDetailBtn = document.getElementById("index-btn");
+const resetBtn = document.getElementById("refresh-btn");
 
 const sidelow = document.querySelectorAll(".side-side-current a");
 for(let i=0; i<sidelow.length; i++){
@@ -9,165 +11,215 @@ for(let i=0; i<sidelow.length; i++){
     }
 }
 
-searchBtn.addEventListener("click", ()=>{
+if(searchBtn != null){
 
-    const options = document.querySelectorAll("#key > option");
+    searchBtn.addEventListener("click", ()=>{
 
-    let key;
-    for(let o of options){
-        if(o.selected){
-            key = o.value;
+        const options = document.querySelectorAll("#key > option");
+
+        let key;
+        for(let o of options){
+            if(o.selected){
+                key = o.value;
+            }
         }
-    }
 
-    if(query.value.trim().length != 0){
-        fetch("/book/search?query=" + query.value + 
-            "&key=" + key + "&memberNo=" + loginMemberNo)
-        .then(resp => resp.json())
-        .then(result => {
+        if(query.value.trim().length != 0){
+            fetch("/book/search?query=" + query.value + 
+                "&key=" + key + "&memberNo=" + loginMemberNo)
+            .then(resp => resp.json())
+            .then(result => {
+                showBookList(result);
+            })
+            .catch(e => console.log(e))
+        }
+    })
 
-            const bList = result.bookList;
-            const lList = result.likeList;
+}
 
-            queryResult.innerText = ""
-            
-            if(bList.length == 0){
-                return;
+if(searchDetailBtn != null){
+
+    searchDetailBtn.addEventListener("click", ()=>{
+        let flag = true;
+        const inputDetail = document.getElementsByClassName("inputDetail");
+        for(let i=0; i<inputDetail.length; i++){
+            if(inputDetail[i].value.trim().length != 0){
+                flag = false;
             }
+        }
+    
+        if(flag){
+            alert("검색어를 입력해주세요");
+            return;
+        }
 
-            for(let b of bList){
-                const bookRow = document.createElement("div");
-                bookRow.classList.add("book-row");
+        const options = document.querySelectorAll("#searchPrint > option");
 
-                const bookInfoWapper = document.createElement("div");
-                bookInfoWapper.classList.add("book-info-wapper");
-
-                const thumbnail = document.createElement("img");
-                thumbnail.setAttribute("src", `${b.thumbnail}`);
-
-                const bookInfo = document.createElement("div");
-                bookInfo.classList.add("book-info");
-
-                const span = document.createElement("span");
-                if(lList.indexOf(b.bookNo) == -1){
-                    span.innerText = "☆";
-
-                }else{
-                    span.innerText = "★";
-                }
-
-                const p1 = document.createElement("p");
-                p1.innerText = `${b.bookTitle}`;
-                const p2 = document.createElement("p");
-                p2.innerText = `${b.bookAuthor}`
-                const p3 = document.createElement("p");
-                p3.innerText = `${b.isbn}`;
-                const p4 = document.createElement("p");
-                p4.innerText = `책상태 : `;
-                
-                const borrowText = document.createElement("span");
-
-                if(b.bookState == 'I' || b.returnDueDate != null){
-                    borrowText.style.color = "red";
-                    borrowText.innerText = "대출 불가능";
-                    p4.append(borrowText);
-
-                    if(b.returnDueDate != null){
-
-                        const resvInfoBtn = document.createElement("button");
-                        resvInfoBtn.classList.add("resvInfoBtn");
-                        resvInfoBtn.innerText = `예약 / 소장정보`;
-                        resvInfoBtn.addEventListener("click", e=>{
-                            const table = e.target.parentElement.parentElement.parentElement.nextElementSibling;
-                            if(table.style.display == "none"){
-                                table.style.display = "table";
-                            }else{
-                                table.style.display = "none";
-                            }
-                        });
-                        const i = document.createElement("i");
-                        i.classList.add("fa-solid", "fa-caret-down");
-                        resvInfoBtn.append(i);
-
-                        p4.append(resvInfoBtn);
-                    }
-                    
-                }else{
-                    borrowText.style.color = "green";
-                    borrowText.innerText = "대출 가능";
-                    p4.append(borrowText);
-                }
-
-                bookInfo.append(p1, p2, p3, p4);
-
-                bookInfoWapper.append(thumbnail, bookInfo, span);
-
-                bookRow.append(bookInfoWapper);
-
-                queryResult.append(bookRow);
-
-                if(b.bookState == 'I' || b.returnDueDate != null){
-                    const table = document.createElement("table");
-                    table.setAttribute("style", "display : none");
-                    
-                    const thead = document.createElement("thead");
-                    const tbody = document.createElement("tbody");
-
-                    const theadTr = document.createElement("tr");
-
-                    const th1 = document.createElement("th");
-                    th1.innerText = "도서관";
-                    const th2 = document.createElement("th");
-                    th2.innerText = "대출상태";
-                    const th3 = document.createElement("th");
-                    th3.innerText = "반납 예정일";
-                    const th4 = document.createElement("th");
-                    th4.innerText = "예약";
-                    const th5 = document.createElement("th");
-                    th5.innerText = "부록";
-                    const th6 = document.createElement("th");
-                    th6.innerText = "비고";
-
-                    const tbodyTr = document.createElement("tr");
-
-                    const td1 = document.createElement("td");
-                    td1.innerText = `JAVA LIBRARY`;
-                    const td2 = document.createElement("td");
-                    td2.innerText = `대출중
-                                     예약 가능
-                                     (예약 : ${b.resvCount}명)`;
-                    const td3 = document.createElement("td");
-                    td3.innerText = `${(b.returnDueDate)}`;
-                    const td4 = document.createElement("td");
-                    const td5 = document.createElement("td");
-                    td5.innerText = `${(b.bookSupple == 'N' ? "없음" : "있음")}`;
-                    const td6 = document.createElement("td");
-                    td6.innerText = ``;
-                    
-                    const resvBtn = document.createElement("button");
-                    resvBtn.innerText = "예약하기";
-                    resvBtn.classList.add("resvBtn")
-                    resvBtn.addEventListener("click", ()=>{showModal(b)});
-
-                    td4.append(resvBtn);
-
-                    theadTr.append(th1, th2, th3, th4, th5, th6);
-                    tbodyTr.append(td1, td2, td3, td4, td5, td6);
-
-                    thead.append(theadTr);
-                    tbody.append(tbodyTr);
-
-                    table.append(thead, tbody);
-
-                    bookRow.append(table);
-
-                }
-                
+        let limit;
+        for(let o of options){
+            if(o.selected){
+                limit = o.value;
             }
+        }
+
+        fetch("/book/search?query=" + inputDetail[0].value + 
+            "&author=" + inputDetail[1].value +
+            "&pub=" + inputDetail[2].value +
+            "&startYear=" + inputDetail[3].value +
+            "&endYear=" + inputDetail[4].value +
+            "&limit=" + limit +
+            "&memberNo=" + loginMemberNo)
+        .then(resp=>resp.json())
+        .then(result=>{
+            showBookList(result);
         })
-        .catch(e => console.log(e))
+        .catch(e=>console.log(e))
+
+    })
+
+}
+
+function showBookList(result){
+
+    const bList = result.bookList;
+    const lList = result.likeList;
+
+    queryResult.innerText = ""
+    
+    if(bList.length == 0){
+        return;
     }
-})
+
+    for(let b of bList){
+        const bookRow = document.createElement("div");
+        bookRow.classList.add("book-row");
+
+        const bookInfoWapper = document.createElement("div");
+        bookInfoWapper.classList.add("book-info-wapper");
+
+        const thumbnail = document.createElement("img");
+        thumbnail.setAttribute("src", `${b.thumbnail}`);
+
+        const bookInfo = document.createElement("div");
+        bookInfo.classList.add("book-info");
+
+        const span = document.createElement("span");
+        if(lList.indexOf(b.bookNo) == -1){
+            span.innerText = "☆";
+
+        }else{
+            span.innerText = "★";
+        }
+
+        const p1 = document.createElement("p");
+        p1.innerText = `${b.bookTitle}`;
+        const p2 = document.createElement("p");
+        p2.innerText = `${b.bookAuthor}`
+        const p3 = document.createElement("p");
+        p3.innerText = `${b.isbn}`;
+        const p4 = document.createElement("p");
+        p4.innerText = `책상태 : `;
+        
+        const borrowText = document.createElement("span");
+
+        if(b.bookState == 'I' || b.returnDueDate != null){
+            borrowText.style.color = "red";
+            borrowText.innerText = "대출 불가능";
+            p4.append(borrowText);
+
+            if(b.returnDueDate != null){
+
+                const resvInfoBtn = document.createElement("button");
+                resvInfoBtn.classList.add("resvInfoBtn");
+                resvInfoBtn.innerText = `예약 / 소장정보`;
+                resvInfoBtn.addEventListener("click", e=>{
+                    const table = e.target.parentElement.parentElement.parentElement.nextElementSibling;
+                    if(table.style.display == "none"){
+                        table.style.display = "table";
+                    }else{
+                        table.style.display = "none";
+                    }
+                });
+                const i = document.createElement("i");
+                i.classList.add("fa-solid", "fa-caret-down");
+                resvInfoBtn.append(i);
+
+                p4.append(resvInfoBtn);
+            }
+            
+        }else{
+            borrowText.style.color = "green";
+            borrowText.innerText = "대출 가능";
+            p4.append(borrowText);
+        }
+
+        bookInfo.append(p1, p2, p3, p4);
+
+        bookInfoWapper.append(thumbnail, bookInfo, span);
+
+        bookRow.append(bookInfoWapper);
+
+        queryResult.append(bookRow);
+
+        if(b.bookState == 'I' || b.returnDueDate != null){
+            const table = document.createElement("table");
+            table.setAttribute("style", "display : none");
+            
+            const thead = document.createElement("thead");
+            const tbody = document.createElement("tbody");
+
+            const theadTr = document.createElement("tr");
+
+            const th1 = document.createElement("th");
+            th1.innerText = "도서관";
+            const th2 = document.createElement("th");
+            th2.innerText = "대출상태";
+            const th3 = document.createElement("th");
+            th3.innerText = "반납 예정일";
+            const th4 = document.createElement("th");
+            th4.innerText = "예약";
+            const th5 = document.createElement("th");
+            th5.innerText = "부록";
+            const th6 = document.createElement("th");
+            th6.innerText = "비고";
+
+            const tbodyTr = document.createElement("tr");
+
+            const td1 = document.createElement("td");
+            td1.innerText = `JAVA LIBRARY`;
+            const td2 = document.createElement("td");
+            td2.innerText = `대출중
+                                예약 가능
+                                (예약 : ${b.resvCount}명)`;
+            const td3 = document.createElement("td");
+            td3.innerText = `${(b.returnDueDate)}`;
+            const td4 = document.createElement("td");
+            const td5 = document.createElement("td");
+            td5.innerText = `${(b.bookSupple == 'N' ? "없음" : "있음")}`;
+            const td6 = document.createElement("td");
+            td6.innerText = ``;
+            
+            const resvBtn = document.createElement("button");
+            resvBtn.innerText = "예약하기";
+            resvBtn.classList.add("resvBtn")
+            resvBtn.addEventListener("click", ()=>{showModal(b)});
+
+            td4.append(resvBtn);
+
+            theadTr.append(th1, th2, th3, th4, th5, th6);
+            tbodyTr.append(td1, td2, td3, td4, td5, td6);
+
+            thead.append(theadTr);
+            tbody.append(tbodyTr);
+
+            table.append(thead, tbody);
+
+            bookRow.append(table);
+
+        }
+        
+    }
+}
 
 const modal = document.getElementById("popup_layer");
 function showModal(book){
@@ -217,14 +269,14 @@ function addReservation(bookNo){
 }
 
 // swiper
-const sampleSlider = new Swiper('.sample', {
+/*const sampleSlider = new Swiper('.sample', {
     slidesPerView: 3,
     spaceBetween: 10,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev"
     }
-})
+})*/
 
 
 
