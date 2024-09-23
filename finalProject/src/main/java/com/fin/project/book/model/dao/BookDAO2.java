59a0994f6e201b9a28349036a20790860cf79ba8
재원 <jw140@naver.com>
2024-09-23 15:19:32 +0900

    이재원 - 희망 도서 관리자 승인 거절

--- finalProject/src/main/java/com/fin/project/book/model/dao/BookDAO2.java
+++ finalProject/src/main/java/com/fin/project/book/model/dao/BookDAO2.java
@@ -1,5 +1,7 @@
 package com.fin.project.book.model.dao;
 
+import java.util.Map;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -9,5 +11,13 @@ public class BookDAO2 {
 
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+
+	public int update(Map<String, Object> paramMap) {
+		return sqlSession.update("bookMapper.updateRequest", paramMap);
+	}
+
+	public int approve(Map<String, Object> paramMap) {
+		return sqlSession.update("bookMapper.updateApprove", paramMap);
+	}
 	
 }
--- finalProject/src/main/java/com/fin/project/book/model/service/BookService2.java
+++ finalProject/src/main/java/com/fin/project/book/model/service/BookService2.java
@@ -1,5 +1,11 @@
 package com.fin.project.book.model.service;
 
+import java.util.Map;
+
 public interface BookService2 {
+
+	int update(Map<String, Object> paramMap);
+
+	int approve(Map<String, Object> paramMap);
 
 }
